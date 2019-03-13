c8d681aef6329929846f91f08c21cc8193b73589
Christian Iuga <ciuga@embl.fr>
2019-03-13 14:06:02 +0100

    To InfluxDB now is working

--- _README.md
+++ _README.md
@@ -1,3 +1,5 @@
 # Command line
 
 `PHONE_ID=... python main.py`
+`DATE=2019-03-08 PHONE_ID=... python main.py`
+`for i in $(seq 1 7) ; do DATE=2019-03-0${i} PHONE_ID=... python3 main.py ; done`
--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from time import sleep
 from random import uniform
+from influxdb import InfluxDBClient
+from dateutil import parser
 
 _url = 'https://measurements.mobile-alerts.eu'
 _phoneId = os.environ.get('PHONE_ID')
+_date = os.environ.get('DATE')
 _headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0'}
+_db_user = 'admin'
+_db_passwd = 'qUV4ingbQLJx!@845CBuo'
+_db_name = 'mobilealerts'
 
 def getSensorName(sensor):
     if 'date' in sensor.text:
@@ -18,41 +24,57 @@ def getSensorName(sensor):
     return sensor.text
 
 
-def getSummarySensor(sensor):
-    return {
-        'name': sensor.a.text,
-        'details_url': sensor.a['href'],
-        'components': {getSensorName(component.h5): component.h4.text
-                       for component in sensor.find_all('div', 'sensor-component')},
-        'last_24h': getDetail(sensor.a['href']),
-    }
 
 
-def getSummary():
+def getSummary(date):
     r = requests.get(
         '{}/Home/SensorsOverview?phoneid={}'.format(_url, _phoneId), headers=_headers)
-    return [getSummarySensor(sensor)
+    return [getDetail(sensor,date)
             for sensor in BeautifulSoup(r.text, 'html.parser').find_all('div', 'sensor')]
 
 
-def getDetail(url,date = None):
-    if date is None:
-        date = datetime.now() - timedelta(days=1)
-    now = datetime.now()
-    fromepoch = int(date.replace(hour=0,minute=1,second=0,microsecond=0).timestamp())
-    toecpoch = int(date.replace(hour=23,minute=59,second=59,microsecond=999999).timestamp())
-    sleep (uniform(1, 3)) # Time in seconds. random between 1 & 3
-    r = requests.post('{}{}'.format(_url, url), data={
-                      'fromepoch': fromepoch, 'toepoch': toecpoch, 'pagesize': 250}, headers=_headers)
+
+
+
+def getDetail(sensor, date = None):
+    sleep (uniform(1, 3))
+    post_data = {
+        'fromepoch': int(date.replace(hour=0,minute=1,second=0,microsecond=0).timestamp()),
+        'toepoch': int(date.replace(hour=23,minute=59,second=59,microsecond=999999).timestamp()),
+        'pagesize': 250
+    }
+    r = requests.post('{}{}'.format(_url, sensor.a['href']), data=post_data, headers=_headers)
 
     soup = BeautifulSoup(r.text, 'html.parser').find(
         'form', {'id': 'MeasurementDetails'})
 
-    header = [getSensorName(h) for h in soup.select('table thead tr th')]
+    header = [getSensorName(h) for h in soup.select(
+        'table thead tr th:nth-child(1n+2)')]
 
-    return [{header[i]: td.text for i, td in enumerate(line.find_all('td'))}
-                   for line in soup.select('table tbody tr')]
+    for line in soup.select('table tbody tr'):
+        _date2 = datetime.strptime(line.select('td')[0].text, '%d/%m/%Y %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%SZ')
+        publishIntoDb([{
+            "measurement": "mobilealerts",
+            "fields": {header[i]: td.text.strip('%C ') for i, td in enumerate(line.select('td:nth-child(1n+2)'))},
+            "tags": {
+                "location": sensor.a.text,
+            },
+            "time": _date2,
+        }])
+
+
+def publishIntoDb(data):
+    print(data)
+    # TODO: write here code to publish into influxDb
+    client = InfluxDBClient(host='localhost', port=8086, username=_db_user , password=_db_passwd )
+    client.switch_database(_db_name)
+    client.write_points(data, database='mobilealerts', time_precision='s')
+
 
 
 if __name__ == "__main__":
-    print(getSummary())
+    if _date is None:
+        date = datetime.now() - timedelta(days=1)
+    else:
+        date = parser.parse(_date)
+    getSummary(date)
